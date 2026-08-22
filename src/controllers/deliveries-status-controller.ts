ba7b3943905c8cdfea453bea7d9d3a61a479620d
Itamar Junior <100168621+ItamarJuniorDEV@users.noreply.github.com>
2026-08-21 22:34:42 -0300

    fix: tornar atualização de status atômica

--- src/controllers/deliveries-status-controller.ts
+++ src/controllers/deliveries-status-controller.ts
@@ -1,39 +1,36 @@
-import { Request , Response } from "express";
+import { Request, Response } from "express";
 import { prisma } from "@/database/prisma";
 import { z } from "zod";
-import { resolve } from "path";
 
 class DeliveriesStatusController {
   async update(req: Request, res: Response) {
     const paramsSchema = z.object({
       id: z.string().uuid(),
-    })
-  
-    const bodySchema = z.object({
-      status: z.enum(["processing", "shipped", "delivered"]),
-    })
-  
-    const { id } = paramsSchema.parse(req.params);
-    const { status } = bodySchema.parse(req.body);
-  
-    await prisma.delivery.update({
-      data: {
-        status,
-      },
-      where: {
-        id, 
-      },
     });
 
-    await prisma.deliveryLog.create({
-      data: {
-        deliveryId: id,
-        description: status
-      }
-    })
-  
+    const bodySchema = z.object({
+      status: z.enum(["processing", "shipped", "delivered"]),
+    });
+
+    const { id } = paramsSchema.parse(req.params);
+    const { status } = bodySchema.parse(req.body);
+
+    await prisma.$transaction(async (tx) => {
+      await tx.delivery.update({
+        data: { status },
+        where: { id },
+      });
+
+      await tx.deliveryLog.create({
+        data: {
+          deliveryId: id,
+          description: status,
+        },
+      });
+    });
+
     return res.json();
   }
 }
 
-export { DeliveriesStatusController }
+export { DeliveriesStatusController };
